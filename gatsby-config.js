234f9dffb51f5111914b3fe4d8080066885e484e
Ryan Furrer <rfurrertech@gmail.com>
2021-07-12 18:17:27 -0400

    Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,27 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-twitter`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-embedder`,
+            options: {
+              customTransformers: [
+                // Your custom transformers
+              ],
+              services: {
+                // The service-specific options by the name of the service
+              },
+            },
+          },
+
+          // Other plugins here...
+        ],
+      },
+    },
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
